0e2f470fe51c202b92e4f5d93d92044e53d9acbf
Fatemeh <fatemehfeizipur@gmail.com>
2026-07-15 01:13:24 -0700

    Add main & state files

---
+++ terraform-portfolio-project/terraform-nextjs-blog/terraform.js/main.tf
@@ -0,0 +1,111 @@
+provider "aws" {
+    region = "ca-canada-1"
+}
+
+# S3 Bucket
+resource "aws_s3_bucket" "nextjs_bucket"{
+    bucket ="nextjs-portfolio-bucket-ff"
+}
+
+
+# Ownership Control
+resource "aws_s3_bucket_ownership_controls" "nextjs_bucket_ownership_control" {
+    bucket = aws_s3_bucket.nextjs_bucket.id
+
+    rule {
+        object_ownership = "BucketOwnerPreferred"
+    }
+}
+
+# Block public Access
+resource "aws_s3_bucket_public_access_block" "nextjs_bucket_public_access_block" {
+    bucket = aws_s3_bucket.nextjs_bucket.id
+
+    block_public_acls = false
+    block_public_policy = false
+    ignore_public_acls = false
+    restrict_public_buckets = false 
+
+}
+
+# Bucket ACL
+resource "aws_s3_bucket_acl" "nextjs_bucket_acl" {
+
+    depends_on = [
+        aws_s3_bucket_ownership_controls.nextjs_bucket_ownership_controls,
+        aws_s3_bucket_public_access_block.nextjs_bucket_public_access_block
+       ]
+    bucket = aws_s3_bucket.nextjs_bucket.id
+    acl = "public-read"
+}
+
+#bucket policy
+
+resource "aws_s3_bucket_policy" "nextjs_bucket-policy" {
+    bucket = aws_s3_bucket.nextjs_bucket.id
+
+    policy = jsonencode (({
+        Version = "2012-10-17"
+        Statement = [
+            {
+                Sid = "PublicReadGetObject"
+                Effect = "Allow"
+                Principal = "*"
+                Action = "s3:GetObject"
+                Resource = "${aws_s3_bucket.nextjs_bucket.arn}/*"
+
+            }
+        ]
+    }))
+}
+
+# Origin Access Identity
+resource "aws_cloudfront_origin_access_identity" "origin_access_identity" {
+    comment ="OAI for Next.js portfolio site"
+}
+
+#Cloudfront distribution
+resource "aws_cloudfront_distribution" "nextjs_distribution" {
+
+    origin {
+        domain_name = aws_s3_bucket.nextjs_bucket.bucket-regional_domain_name,
+        origin_id = "S3-nextjs-portfolio-bucket"
+
+        s3_origin_config{
+            origin_access_identity = aws_cloudfront_origin_access_identity.origin_access_identity.cloudfront_access_identity_path
+        }
+    }
+
+    enabled = true
+    is_ipv6_enabled = true
+    comment = "Next.js potyfolio site"
+    default_root_object = "index.html"
+
+
+    default_cache_behavior {
+        allowed_methods = ["GET" , "HEAD", "POTIONS"]
+        cached_methods = ["GET", "HEAD"]
+        target_origin_id = "S3-nextjs-portfolio-bucket"
+
+        forwarded_values {
+            query_string = false
+            cookies {
+                forward = "none"
+            }
+        }
+
+        viewer_protocol_policy = "redirect-to-https"
+        min_ttl = 0
+        default_ttl = 3600
+        max_ttl = 86400
+    }
+
+    viewer_certificate {
+
+    }
+    restrictions {
+        geo_restriction {
+            restriction_type = "none" 
+        }
+    }
+}
--- terraform-portfolio-project/terraform-nextjs-blog/terraform.js/state.tf
+++ terraform-portfolio-project/terraform-nextjs-blog/terraform.js/state.tf
@@ -1,60 +1,10 @@
 terraform {
     backend "s3" {
-       bucket = "ff-my-terraform-state"
+       bucket = ""
        key = "global/s3/terraform.tfstate"
        region = "ca-central-1"
-       dynamodb_table = "terraform-lock-file"
+       dynamodb_table = ""
 
     } 
 }
 
-# Ownership Control
-resource "aws_s3_bucket_ownership_controls" "nextjs_bucket_ownership_control" {
-    bucket = aws_s3_bucket.nextjs_bucket.id
-
-    rule {
-        object_ownership = "BucketOwnerPrefered"
-    }
-}
-
-# Block public Access
-resource "aws_s3_bucket_public_access_block" "nextjs_bucket_public_access_block" {
-    bucket = aws_s3_bucket.nextjs_bucket.id
-
-    block_public_acls = false
-    block_public_policy = false
-    ignore_public_acls = false
-    restrict_public_buckets = false 
-
-}
-
-# Bucket ACL
-resource "aws_s3_bucket_acl" "nextjs_bucket_acl" {
-
-    depends_on = [
-        aws_s3_bucket_ownership_controls.nextjs,
-        aws_s3_bucket_public_access_block.nextjs_bucket_public_access_block
-       ]
-    bucket = aws_s3_bucket.nextjs_bucket.id
-    acl = "public-read"
-}
-
-#bucket policy
-
-resource "aws_s3_bucket_policy" "nextjs_bucket-policy" {
-    bucket = aws_s3_bucket.nextjs_bucket.id
-
-    policy = jsondecode(({
-        version = "2012-10-17"
-        Statement = [
-            {
-                Sid = "PublicReadGetObject"
-                Effect = "Allow"
-                Principal = "*"
-                Action = "s3:GetObject"
-                Resource = "${aws_s3_bucket.nextjs_bucket.arn}/*"
-
-            }
-        ]
-    }))
-}
